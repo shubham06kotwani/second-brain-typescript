9a25b34e8ccb782373fe749ddd6de9fde90c5a75
shubham06kotwani <shubhamkotwani259@gmail.com>
2026-09-21 01:54:00 +0530

    feat: initialize TypeScript project with Express backend
    - Added package.json for project dependencies and scripts.
    - Created config.ts for JWT password configuration.
    - Implemented db.ts for MongoDB connection and user/content models.
    - Defined custom Express Request type in express.d.ts.
    - Developed main application logic in index.ts with user signup/signin and content management routes.
    - Added user authentication middleware in middleware.ts.
    - Implemented utility function for generating random strings in utils.ts.
    - Configured TypeScript settings in tsconfig.json and generated tsbuildinfo.

---
+++ src/backend/middleware.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config.js";
+
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const decoded = jwt.verify(header as string, JWT_PASSWORD)
+    if (decoded) {
+        if (typeof decoded === "string") {
+            res.status(403).json({
+                message: "You are not logged in"
+            })
+            return;    
+        }
+        req.userId = (decoded as JwtPayload).id;
+        next()
+    } else {
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
